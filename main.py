3f22636866729c3e5d359da1c4a442dd5f5ad0c1
sidymohamed12 <mohamedsaizonou86@gmail.com>
2026-06-18 20:31:44 +0000

    ajout de commentaire

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 import pandas as pnd;
 import numpy as np;
+
+
+# gestion de stock
+#  fonctionnalité : 
+    # - ajouter produit   ==> khalil
+    # - lister produit    ==> Omar 
+    # - modifier produit  ==> Sidy
+    # - supprimer produit
+    # - ajouter un client => khalil
+    # - supprimer un cient => Omar
+    
+
+
+# 2 types d'approches
+# - 1ere approche : chacun cree sa branche et y travaille
+    #  - dev/omar
+    #  - dev/khalil
+    #  - dev/sidy
+
+
+
+# - 2e approche : une branche par fonctionnalite
+    #  - feat/lister-produit  => Omar
+    #  - feat/add-product  => khalil
+    #  - feat/update-product => Sidy
+    
+
+# cree une branche dev : branche en commun
+# chacun va merge son travail dans dev    
+    
+# feat -> feature ? fonnctionnalité
